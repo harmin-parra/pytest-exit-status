4fc35a27dd8c26fa9cf5e19ed12a7f1455c33b43
harmin-parra <harmin.parra@gmail.com>
2025-01-29 22:58:44 +0100

    complete test case

--- test/xfailed_test.py
+++ test/xfailed_test.py
@@ -4,6 +4,13 @@ import pytest
 @pytest.mark.xfail(reason="xfailed")
 def test_1():
     """
-    XFailed
+    XFailed with @pytest.mark.xfail
     """
     assert False
+
+
+def test_2():
+    """
+    XFailed with pytest.xfail
+    """
+    pytest.xfail("xfail")
